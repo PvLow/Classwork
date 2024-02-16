7d1ec9e2c6ab02323782ee8310190493e8d54178
PvLow <156699049+PvLow@users.noreply.github.com>
2024-02-16 21:11:20 +0300

    Дерево

---
+++ 16.02.2024/Main.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+
+using namespace std;
+
+template<typename T>
+ struct Node {
+	    T value;
+	    Node<T>* right = nullptr;
+	    Node<T>* left = nullptr;
+    };
+
+template<typename T>
+class BinaryTree {
+private:
+	
+	Node<T>* root = nullptr;
+	int count = 0;	
+
+	void showAll(Node<T>* el) {
+		cout << el->value<<" ";
+		if (el->left != nullptr) showAll(el->left);
+		if (el->right != nullptr) showAll(el->right);
+	}
+
+public:
+	void add(T value) {
+		
+		Node<T>* newNode = new Node<T>{ value };
+
+		if (root == nullptr) {
+			root = newNode;
+			count++;
+			return;
+		}
+
+		Node<T>* buf = root;
+		while (true) {
+			if (value > buf->value) {
+				if (buf->right == nullptr) {
+					buf->right = newNode;
+					break;
+				}
+				buf = buf->right;
+			}
+			else {
+				if (buf->left == nullptr) {
+					buf->left = newNode;
+					break;
+				}
+				buf = buf->left;
+			}
+		}
+		count++;
+	}
+
+	void show() {
+		
+		if (root != nullptr) showAll(root);
+	}
+
+
+
+};
+
+int main() {
+
+	int ar[]{ 50,27,64,69,18,31,42,80,57 };
+	BinaryTree<int> tree;
+	for (int i = 0; i < 9; i++) {
+		tree.add(ar[i]);
+	}
+	tree.show();
+
+
+}
